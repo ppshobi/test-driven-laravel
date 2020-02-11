7bee87b72355236d45920202a51189e29d47bfa9
Shobi P P <ppshobi@gmail.com>
2020-02-11 18:49:58 +0300

    show only the promoters listing and mix up the concert listings to fix any future bugs

--- app/Http/Controllers/Backstage/ConcertsController.php
+++ app/Http/Controllers/Backstage/ConcertsController.php
@@ -13,7 +13,7 @@ class ConcertsController extends Controller
 {
     public function index()
     {
-        return view('backstage.concerts.index', ['concerts' => Concert::all()]);
+        return view('backstage.concerts.index', ['concerts' => Auth::user()->concerts]);
     }
 
     public function create()
--- tests/Feature/Backstage/ViewConcertListTest.php
+++ tests/Feature/Backstage/ViewConcertListTest.php
@@ -26,17 +26,23 @@ class ViewConcertListTest extends TestCase
     /**
      * @test
      **/
-    function promoter_can_view_a_list_of_their_concerts()
+    function promoter_can_only_view_a_list_of_their_own_concerts()
     {
         $this->disableExceptionHandling();
         $user = factory(User::class)->create();
-        $concerts =  factory(Concert::class, 3)->create(['user_id' => $user->id]);
+        $otherUser = factory(User::class)->create();
+        $concertA =  factory(Concert::class)->create(['user_id' => $user->id]);
+        $concertB =  factory(Concert::class)->create(['user_id' => $user->id]);
+        $concertC =  factory(Concert::class)->create(['user_id' => $otherUser->id]);
+        $concertD=  factory(Concert::class)->create(['user_id' => $user->id]);
+
 
         $response = $this->actingAs($user)->get('/backstage/concerts');
 
         $response->assertStatus(200);
-        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[0]));
-        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[1]));
-        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[2]));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concertA));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concertB));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concertD));
+        $this->assertFalse($response->original->getData()['concerts']->contains($concertC));
     }
 }
