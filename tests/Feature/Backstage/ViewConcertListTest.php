ced92cb38be493c70b5231406dac3aad8404fba7
Shobi P P <ppshobi@gmail.com>
2020-02-11 18:43:11 +0300

    promoter can see their concerts

--- tests/Feature/Backstage/ViewConcertListTest.php
+++ tests/Feature/Backstage/ViewConcertListTest.php
@@ -4,10 +4,14 @@
 namespace Tests\Feature\Backstage;
 
 
+use App\User;
+use App\Concert;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ViewConcertListTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * @test
      **/
@@ -17,5 +21,22 @@ class ViewConcertListTest extends TestCase
 
         $response->assertStatus(302);
         $response->assertRedirect('/login');
+    }
+
+    /**
+     * @test
+     **/
+    function promoter_can_view_a_list_of_their_concerts()
+    {
+        $this->disableExceptionHandling();
+        $user = factory(User::class)->create();
+        $concerts =  factory(Concert::class, 3)->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user)->get('/backstage/concerts');
+
+        $response->assertStatus(200);
+        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[0]));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[1]));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[2]));
     }
 }
